e2df07726ce9eb7125f1974d54c384d39a972ec6
Suman Bogati <suman@vidyamantra.com>
2014-01-16 12:13:52 +0530

    created html structure for message information
    created html structure for message information

--- js/packetcontainer.js
+++ js/packetcontainer.js
@@ -2,22 +2,43 @@
 	function (window){
 		var whBoard = window.whBoard;
 		whBoard.createPacketContainer = function (){
+				
+			var labelDiv = document.createElement('div');
+				labelDiv.id = "dataInformation";
+				
+				document.getElementById('packetContainer').appendChild(labelDiv);
+				
+				var blankDiv = document.createElement('div');
+					blankDiv.id = "blankDiv";
+					
+					labelDiv.appendChild(blankDiv);
+				
+				var perSecLabel = document.createElement('div');
+					perSecLabel.id = "perSecData";
+					perSecLabel.innerHTML = "Per Second Data";
+					
+					labelDiv.appendChild(perSecLabel);
+					
+				var totalDataLabel = document.createElement('div');
+					totalDataLabel.id = "totalDataLabel";
+					totalDataLabel.innerHTML = "Total";
+					labelDiv.appendChild(totalDataLabel);	
+					
+			
 			var packCont  = document.createElement('div');
 				packCont.id = 'sendPackCont';
-			
-				//document.getElementById('canvasWrapper').appendChild(packCont);
 				document.getElementById('packetContainer').appendChild(packCont);
 				
-				//creating div for sending packet per second
+				//
+				var sendPacketPSLabel = document.createElement('div');
+					sendPacketPSLabel.id = 'sentPacketsLabel';
+					sendPacketPSLabel.innerHTML = "Sent Packets";
+					
+					packCont.appendChild(sendPacketPSLabel);
+					
 				var sendPacketPS =  document.createElement('div');
 				sendPacketPS.id = 'sendPackPsCont'; 
 			    packCont.appendChild(sendPacketPS);
-				
-				label = document.createElement('label'); 
-				
-				//label.innerHTML = "Per second sent packets";
-				label.innerHTML =  whBoard.lang.getString('perSecSentPacket');
-				sendPacketPS.appendChild(label);
 				
 				counterDiv = document.createElement('div');
 				counterDiv.id = whBoard.sentPackDivPS;
@@ -32,10 +53,6 @@
 			    packCont.appendChild(totSendPacket);
 			    
 				
-				var label = document.createElement('label'); 
-					//label.innerHTML = "Total Sent Packets";
-					label.innerHTML =  whBoard.lang.getString('totSentPackets');
-					totSendPacket.appendChild(label);
 					
 					var counterDiv = document.createElement('div');
 					counterDiv.id = whBoard.sentPackDiv;
@@ -44,35 +61,25 @@
 					totSendPacket.appendChild(counterDiv);
 					
 				
-//				//TODO below and above statement can be achieved by single funciton
-//				packCont  = document.createElement('div');
-//				packCont.id = 'receivePackCont';
-//				packCont.innerHTML = "Total Received Packets";
-//				document.getElementById('canvasWrapper').appendChild(packCont);
-//				
-//				counterDiv = document.createElement('div');
-//				counterDiv.id = 'receivedNumber';
-//				counterDiv.className = 'numbers';
-//				counterDiv.innerHTML = 0;
-//				packCont.appendChild(counterDiv);
-				
-				
-				//hello
-				 packCont  = document.createElement('div');
+
+				packCont  = document.createElement('div');
 				packCont.id = 'receivePackCont';
-				
-				//document.getElementById('canvasWrapper').appendChild(packCont);
 				document.getElementById('packetContainer').appendChild(packCont);
 				
-				//creating div for sending packet per second
+				var receivedPacketPSLabel = document.createElement('div');
+				receivedPacketPSLabel.id = 'receivedPacketsLabel';
+				receivedPacketPSLabel.innerHTML = "Recevied Packets";
+				
+				packCont.appendChild(receivedPacketPSLabel);
+				
 				var receivePacketPS =  document.createElement('div');
 				receivePacketPS.id = 'receivePackPsCont'; 
 			    packCont.appendChild(receivePacketPS);
 				
+			    /*
 				label = document.createElement('label'); 
 				label.innerHTML =  whBoard.lang.getString('perSecRcvdData');
-				//label.innerHTML = "Per second received packets";
-				receivePacketPS.appendChild(label);
+				receivePacketPS.appendChild(label); */
 				
 				counterDiv = document.createElement('div');
 				counterDiv.id = whBoard.receivedPackDivPS;
@@ -86,11 +93,11 @@
 					totReceivedPack.id = 'totReceivedPackCont'; 
 					packCont.appendChild(totReceivedPack);
 			    
-				
+				/*
 				var label = document.createElement('label'); 
 					//label.innerHTML = "Total Received Packets";
 					label.innerHTML =  whBoard.lang.getString('totRcvdPackets');
-					totReceivedPack.appendChild(label);
+					totReceivedPack.appendChild(label); */
 					
 					counterDiv = document.createElement('div');
 					counterDiv.id = whBoard.receivedPackDiv;
